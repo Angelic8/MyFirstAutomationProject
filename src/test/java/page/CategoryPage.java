4904f0d8bbd9a34694d1bd00209133624287f6dc
belle09 <angeliqueumilda1009@gmail.com>
2021-01-12 18:19:25 +0800

    Added the Extent Reports; the executed test cases are now displayed in the Test Reports on Web

--- src/test/java/page/CategoryPage.java
+++ src/test/java/page/CategoryPage.java
@@ -4,10 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,11 +12,13 @@ public class CategoryPage {
 
 	private WebDriver driver = null;
 	private WebDriverWait wait;
-	private WebElement element;
 	
 	By logo_shopeeMall = By.cssSelector("div._1Bj1VS.qEcSbS:nth-child(2) div.home-page:nth-child(2) div.container div.homepage-mall-section:nth-child(6) div.shopee-header-section.shopee-header-section--simple div.shopee-header-section__header div.shopee-header-section__header__title div._3rH8jc > a.DbfcwV._2KY1GO");
 	By seeAll_shopeeMall = By.cssSelector("div._1Bj1VS.qEcSbS:nth-child(2) div.home-page:nth-child(2) div.container div.homepage-mall-section:nth-child(6) div.shopee-header-section.shopee-header-section--simple div.shopee-header-section__header a.shopee-header-section__header-link button.shopee-button-no-outline a._42RSr7 > div._2Lx4zQ");
-
+	By ads_shopeeMall = By.cssSelector("body.shopee-no-scroll:nth-child(2) div:nth-child(3) div:nth-child(1) div.shopee-popup.shopee-modal__transition-enter-done div.shopee-popup__container > div.shopee-popup__close-btn");
+	By link_shopeeHome = By.cssSelector("div.theme--ofs div._1Bj1VS.hQrkfh:nth-child(2) div.shopee-top.shopee-top--sticky div.navbar-wrapper.container-wrapper.navbar-wrapper--official-shop div.container.navbar div.flex.v-center._1A6Wy3 > a._1UIe83:nth-child(1)");
+	
+	
 	public CategoryPage(WebDriver driver) {
 		this.driver = driver;
 	} // end constructor CategoryPage() with parameter driver
@@ -27,12 +26,10 @@ public class CategoryPage {
 	public void clickLogoSM() {
 	
 		driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
-		
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		
 		jse.executeScript("window.scrollBy(0,1000)", "");
 		
-		element = driver.findElement(logo_shopeeMall);
+		driver.findElement(logo_shopeeMall);
 		
 	} // end method clickLogoSM()
 	
@@ -43,6 +40,27 @@ public class CategoryPage {
 				
 		
 	} // end method clickSeeAllSMLink()
+	
+	public void clickAdsSM() {
+		
+		wait = new WebDriverWait(driver, 20);
+		
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(ads_shopeeMall)).click();	
+		} catch (Exception e) {
+			System.out.println(e.getCause());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} // end try-catch block
+
+	} // end method clickAdsSM()
+	
+	public void clickLinkSH() {
+		
+		wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.elementToBeClickable(link_shopeeHome)).click();
+		
+	} // end method clickLinkSH()
 	
 	
 } // end class CategoryPage
--- src/test/java/test/EcommerceTestCases.java
+++ src/test/java/test/EcommerceTestCases.java
@@ -2,39 +2,54 @@ package test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import page.CategoryPage;
 import page.HomePage;
 import page.LoginPage;
-//import page.SearchAndSearchResults;
 
 public class EcommerceTestCases {
 
-	// 1. create a private instance variable for the driver
+	// 1. Create a private instance variable for the driver
 	private static WebDriver driver = null;
-	
-	// create a private instance variable for pages; data should be inside the test suite
+
+	// This is the private instance variable for site pages; data should be inside the @test
 	private static LoginPage loginPageObj;
 	private static HomePage HomePageObj;
 	private static CategoryPage CategoryPageObj;
-	//private static SearchAndSearchResults SearchAndSearchResultsObj;
-	
+
+	// This is for test reports
+	private static ExtentSparkReporter spark;
+	private static ExtentReports extent;
+	private static ExtentTest test;
+
+	@BeforeSuite
+	public void setUp() {
+
+		spark = new ExtentSparkReporter("ECommerceTestCases.html");
+		extent = new ExtentReports();
+		extent.attachReporter(spark);
+
+	} // end method setUp()
+
 	@BeforeTest
 	public void setUpTest() {
 
-		// 2. get the project location
+		// 2. Get the project location
 		String projectPath = System.getProperty("user.dir");
 
-		// 3. set the Chrome driver and its location
+		// 3. Set the Chrome driver and its location
 		System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver1.exe");
 
 		// Web Push Notifications
@@ -44,61 +59,103 @@ public class EcommerceTestCases {
 		// 1-Allow, 2-Block, 0-default
 		options.setExperimentalOption("prefs", prefs);
 
-		// 4. declare and refer a new Chrome object
+		// 4. Declare and refer a new Chrome object
 		driver = new ChromeDriver(options);
 
 		// Maximizes the window
 		driver.manage().window().maximize();
 
-		// 5. declare and refer a new object to its class, and then pass it to the driver; data is from the pages own driver
+		// 5. Declare and refer a new object to its class, and then pass it to the
+		// driver; data is from the pages own driver
 		loginPageObj = new LoginPage(driver);
 		HomePageObj = new HomePage(driver);
 		CategoryPageObj = new CategoryPage(driver);
-		//SearchAndSearchResultsObj = new SearchAndSearchResults(driver);
-		
-		// 6. get the URL of the website
+
+		// 6. Get the URL of the website
 		driver.get("https://shopee.ph/");
-		
+
 	} // end method setup()
 
 	@Test
 	public static void aLoginValidCredentialsTest() throws InterruptedException {
 
+		test = extent.createTest("Verify Login Page", "Check using valid credentials");	
+		test.log(Status.INFO, "Login");
+		
 		// 7. refer the method from loginPageObj
-		Thread.sleep(2000);
+		Thread.sleep(4000);
 		loginPageObj.clickLoginAdsButton();
+		test.pass("Ads button is clicked.");
+		
 		Thread.sleep(2000);
 		loginPageObj.clickLoginLink();
+		test.pass("Login is clicked.");
+		
 		loginPageObj.setTextInLoginNameBox("gabbieeneko@gmail.com");
+		test.pass("Username field is available.");
+		
 		Thread.sleep(2000);
 		loginPageObj.setTextInLoginPasswordBox("FuRrPasSc0d32021");
+		test.pass("Password field is available.");
+		
 		Thread.sleep(2000);
 		loginPageObj.clickLoginButton();
-
+		test.pass("Successfully Logged-in!");
+		
 	} // end method loginValidCredentialsTest()
 
 	@Test
 	public static void bHomePageTest() throws InterruptedException {
 
+		test = extent.createTest("Verify Homepage","Check the features and functionalities.");
+		test.log(Status.INFO, "Homepage");
+		
 		Thread.sleep(2000);
 		HomePageObj.clickLoginAdsButtonAfter();
+		test.pass("Pop-up Ads on Homepage is clicked.");
+		test.pass("User is in Homepage.");
+		
 		Thread.sleep(2000);
 		HomePageObj.inputSearchBar("sunnies face fluffmatte");
+		test.pass("Search bar is available");
+		
 		Thread.sleep(2000);
-		HomePageObj.clickSearchButton();	
+		HomePageObj.clickSearchButton();
+		test.pass("Search button is clicked.");
+		test.pass("User is directed to the Search Results page.");
+		
 		HomePageObj.clickShopeeLogo();
+		test.pass("Shopee Logo is clicked.");
+		test.pass("User is directed to Homepage.");
+		
 	} // end method bHomePageTest()
-	
+
 	@Test
 	public static void cCategoryPageTest() throws InterruptedException {
+
+		test = extent.createTest("Verify Category Page","Check the features and functionalities.");
+		test.log(Status.INFO, "Category");
 		
 		Thread.sleep(2000);
 		CategoryPageObj.clickLogoSM();
+		test.pass("Shopee Mall section in Homepage is available.");
+		
 		CategoryPageObj.clickSeeAllSMLink();
+		test.pass("See All link is clicked.");
+		test.pass("User is directed to Shoppe Mall page.");
+		
+		Thread.sleep(4000);
+		CategoryPageObj.clickAdsSM();
+		test.pass("Pop-up ads on Shopee Mall is clicked.");
+		test.pass("User is in the Shopee Mall - Category page.");
+		
+		Thread.sleep(2000);
+		CategoryPageObj.clickLinkSH();
+		test.pass("Shopee Home link is clicked.");
+		test.pass("User is directed to Homepage.");
 		
 	} // end method cCategoryPageTest()
-	
-	
+
 	@AfterTest
 	public static void endTest() throws InterruptedException {
 
@@ -106,7 +163,12 @@ public class EcommerceTestCases {
 		Thread.sleep(5000);
 		driver.close();
 		driver.quit();
-
+		test.pass("Browser is closed");
+		test.info("Test has been completed");
+		
+		// This is required when using the Extent Report
+		extent.flush();
+		
 	} // end method endTest()
 
 } // end class EcommerceTestCases
